2863a717c85d3abbb699d02d25981f3071b5f355
Martin Myhre <70258183+NotLimey@users.noreply.github.com>
2023-01-29 23:16:47 +0100

    Navbar search

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
-	import { auth } from '$lib/firebase';
 	import { account } from '$stores/auth.store';
+	import { Icon, Search } from 'svelte-hero-icons';
 </script>
 
 <nav class="w-full max-w-5xl py-4 px-5 flex justify-between mx-auto">
-	<a href="/"> Limeyfy socials </a>
+	<div class="gap-x-12 flex items-center">
+		<a href="/" class="hover:underline"> Limeyfy socials </a>
+		<a href="/s" class="flex items-center gap-x-2 hover:underline"
+			>Search<Icon src={Search} class="h-5 w-5" /></a
+		>
+	</div>
 	<a href="/{$account?.userName}" class="flex items-center gap-x-4">
 		<p>Hi, {$account?.displayName}</p>
 		<img
